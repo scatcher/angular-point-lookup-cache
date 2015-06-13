92fc63fa86594f222d2ccdd69ed4f7bd7e62d467
Scott Hatcher <scott@scotthatcher.com>
2015-06-13 12:10:30 -0700

    Minor bump updated dependencies.

--- typings/ap.d.ts
+++ typings/ap.d.ts
@@ -1,12 +1,4 @@
 /// <reference path="tsd.d.ts" />
 declare module ap {
-	interface ILookupCacheService {
-		cacheEntityByLookupId(listItem: ap.IListItem<any>, propertyArray: string[]): void;
-		cacheSingleLookup(listItem: ap.IListItem<any>, propertyName: string, listId: string): void;
-		getPropertyCache<T>(propertyName: string, listId: string): { [key: number]: ap.IIndexedCache<T> };
-		removeEntityFromLookupCaches(listItem: ap.IListItem<any>, propertyArray: string[]): void;
-		removeEntityFromSingleLookupCache(listItem: ap.IListItem<any>, propertyName: string, listId: string): void;
-        retrieveLookupCacheById<T>(propertyName: string, listId: string, cacheId: number): ap.IIndexedCache<T>;
-        retrieveLookupCacheById<T>(propertyName: string, listId: string, cacheId: number, asObject?: boolean): T[];
-    }
+
 }
